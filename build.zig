c80389ae93e4280a6f9db513c6a0df24b3824dc7
Floogle <18466542+skyfloogle@users.noreply.github.com>
2024-08-11 15:28:43 +0200

    build system

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -8,7 +9,7 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .x86, .os_tag = .windows, .abi = .msvc } });
 
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
@@ -24,10 +25,19 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const artifact_step = b.addInstallArtifact(lib, .{});
+
+    // get gm82gex
+    const gm82gex_download = b.addSystemCommand(&.{ "curl", "https://raw.githubusercontent.com/GM82Project/gm82gex/main/gm82gex.py" });
+    const gm82gex_output = gm82gex_download.captureStdOut();
+    const gm82gex_file = b.addInstallFileWithDir(gm82gex_output, .prefix, "gm82gex.py");
+
+    const gm82gex_run = b.addSystemCommand(&.{if (builtin.os.tag == .windows) "py" else "python3"});
+    gm82gex_run.addArg(b.getInstallPath(gm82gex_file.dir, gm82gex_file.dest_rel_path));
+    gm82gex_run.addFileArg(.{ .src_path = .{ .owner = b, .sub_path = "gm82gltf.gej" } });
+    gm82gex_run.step.dependOn(&gm82gex_file.step);
+    gm82gex_run.step.dependOn(&artifact_step.step);
+    b.getInstallStep().dependOn(&gm82gex_run.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
